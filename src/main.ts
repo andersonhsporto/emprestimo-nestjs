c218d0b027e56a314d5c1fbaff597ca4f8ba87a2
Anderson Porto <anderson.higo2@gmail.com>
2023-04-23 04:37:59 -0300

    add: cors support

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,12 @@ import {ValidationPipe} from "@nestjs/common";
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.useGlobalPipes(new ValidationPipe());
+    app.enableCors(
+        {
+            origin: "*",
+            methods: "GET,PUT,POST,DELETE",
+        }
+    );
     await app.listen( process.env.PORT || 3000);
 }
 
